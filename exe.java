0d6c9041be7873961a2138c7437566f9f6e12e28
Oldent <102659623+oldent0-0@users.noreply.github.com>
2024-04-27 12:14:26 +0800

    Changes: - Added Scanner & value.

--- exe.java
+++ exe.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class exe {
     public static void main(String[] args) {
         System.out.println("hello tnedlo");
+        Scanner scanner = new Scanner(System.in);
+        int value = Integer.parseInt(scanner.nextLine());
+        System.out.println(value);
     }
 }
